a476c69114b4323bd06a433c5676e5d1cf8e3ddf
Carolyn B <bmathematics@gmail.com>
2022-07-02 10:25:06 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-import About from "./components/About";
-import Nav from "./components/Nav";
-import Gallery from "./components/Gallery";
-import ContactForm from "./components/Contact";
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
     {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -28,13 +25,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
           <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        )}
       </main>
     </div>
   );
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import PhotoList from "../PhotoList";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery(props) {
   const { currentCategory } = props;
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.name}</p>
+      <p>{currentCategory.description}</p>
       <PhotoList category={currentCategory.name} />
     </section>
   );
